8b259766fde78023af34238a89e42c9ffb31af7e
SAI DUTT <duttganduri46@gmail.com>
2021-12-17 02:48:11 -0500

    added hub and update method, needs refactor on this logic

--- WebApi/Hubs/RoomHub.cs
+++ WebApi/Hubs/RoomHub.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Domain.Entities;
 using DtoModels.Requests;
+using Infrastructure.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
 using Persistence.Context;
@@ -11,24 +12,28 @@ namespace WebApi.Hubs
     public class RoomHub : Hub
     {
         private readonly ScrumPokerContext _context;
+        private readonly IRoomService _roomService;
         private readonly UserManager<ApplicationUser> _userManager;
-        public RoomHub(ScrumPokerContext context, UserManager<ApplicationUser> userManager)
+        public RoomHub(ScrumPokerContext context, IRoomService roomService ,UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _roomService = roomService;
             _userManager = userManager;
         }
 
-        //public async Task JoinRoom(JoinRoomRequest request)
-        //{
-        //    await Groups.AddToGroupAsync(Context.ConnectionId, request.RoomId.ToString());
-        //    ApplicationUser user = await _userManager.FindByIdAsync(request.UserId.ToString());
-        //    user.ConnectionId = Context.ConnectionId;
-        //    user.RoomId = request.RoomId;
-        //    await _userManager.UpdateAsync(user);
-        //    db.Users.Add(user);
-        //    await db.SaveChangesAsync();
-        //    await Clients.Caller.SendAsync("Self", user);
-        //    await Clients.Group(request.RoomId).SendAsync("UserJoined", new ListChange<User>(user, await GetRoomUsers(request.RoomId)));
-        //}
+        public async Task JoinRoom(JoinRoomRequest request)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, request.RoomId.ToString());
+            ApplicationUser user = await _userManager.FindByIdAsync(request.UserId.ToString());
+            user.ConnectionId = Context.ConnectionId;
+            var room = await _roomService.GetById(request.RoomId);
+            room.Payload.Users.Add(user);
+            await _roomService.Update(request.RoomId, room.Payload);
+            await _userManager.UpdateAsync(user);
+            await _context.SaveChangesAsync();
+            await Clients.Caller.SendAsync("Self", user);
+            await Clients.Group(request.RoomId.ToString()).SendAsync("UserJoined", user);
+
+        }
     }
 }
